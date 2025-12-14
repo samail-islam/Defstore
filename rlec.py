ea8189dbc48ce5816301af44f809a764ceb3d491
Samail Islam <samailislam71@gmail.com>
2025-12-14 23:17:16 +0600

    Change compression output to print statements
    Refactor compression output to print directly instead of returning formatted strings.

--- rlec.py
+++ rlec.py
@@ -139,12 +139,11 @@ class RLECompressor:
 
         ratio = (1 - comp_size / orig_size) * 100 if orig_size else 0
 
-        return (
-            f"Compressed → {compressed}\n",
-            Fore.CYAN +f"Original size: {orig_size} bytes\n",
-            Fore.CYAN +f"Compressed size: {comp_size} bytes\n",
-            Fore.YELLOW +f"Compression ratio: {ratio:.2f}%"
-        )
+        print(f"Compressed → {compressed}")
+        print(Fore.CYAN +f"Original size: {orig_size} bytes")
+        print(Fore.CYAN +f"Compressed size: {comp_size} bytes")
+        print(Fore.YELLOW +f"Compression ratio: {ratio:.2f}%")
+        
 
     # ---------- COMMAND HANDLER ----------
     def run(self):
